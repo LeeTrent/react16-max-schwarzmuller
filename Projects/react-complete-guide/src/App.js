3b1a11c7a4c92a03e40671f9469dfc642126f2c6
LeeTrent <Lee.Trent.1@gmail.com>
2018-10-17 16:23:31 -0500

    Replaces the use of Radium with CSS Modules for CSS rendering

--- Projects/react-complete-guide/src/App.js
+++ Projects/react-complete-guide/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
@@ -49,16 +49,9 @@ class App extends Component {
 
   render() {
 
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid black',
-      padding: '8px',
-      curosr: 'pointer',
-     };
-
     let persons = null;
+    let btnClass = '';
+
     if ( this.state.showPersons ) {
       persons = (
           <div>
@@ -74,8 +67,7 @@ class App extends Component {
             }
         </div>         
       );
-
-      style.backgroundColor = 'red';
+      btnClass = classes.Red;
     }
 
     // The join(' ') method will convert 
@@ -84,20 +76,20 @@ class App extends Component {
     // (i.e. 'red bold')
     //let classes = ['red', 'bold'].join(' ');
 
-    let classes = [];
+    let assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red'); // classes = ['red']
+      assignedClasses.push(classes.red); // classes = ['red']
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold'); // classes = ['red', 'bold']
+      assignedClasses.push(classes.bold); // classes = ['red', 'bold']
     }    
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>This is a React 16.5.2 Application</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
+        <p className={assignedClasses.join(' ')}>This is really working!</p>
         <button
-          style={style}
+          className={btnClass}
           onClick={this.togglePersonsHandler}>Toggle Names
         </button>
         {persons}
