a97b3d465341971c5f04af1a8140fc6918800d8d
LeeTrent <Lee.Trent.1@gmail.com>
2018-11-05 17:09:41 -0600

    Adds lifecycle methods to Persons.js in response to 'external' property changes

--- Projects/react-complete-guide/src/components/Persons/Persons.js
+++ Projects/react-complete-guide/src/components/Persons/Persons.js
@@ -15,6 +15,22 @@ class Persons extends Component {
       console.log("[Persons.js][componentDidMount]");
    }
 
+   componentWillReceiveProps(nextProps) {
+      console.log("[Persons.js][componentWillReceiveProps]", nextProps);
+   }
+
+   shouldComponentUpdate(nextProps, nextState) {
+      console.log("[Persons.js][shouldComponentUpdate][nextProps]", nextProps, nextState);
+      return nextProps.persons !== this.props.persons;
+   }   
+
+   componentWillUpdate(nextProps, nextState) {
+      console.log("[Persons.js][componentWillUpdate][nextProps]", nextProps, nextState);
+   }
+
+   componentDidUpdate() {
+      console.log("[Persons.js][componentDidUpdate]");
+   }
    render() {
       console.log("[Persons.js][render]");
       return this.props.persons.map( (person, index) => {
